dc38dd18e549ce974b0b46b714a806a80b098be5
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-21 12:35:56 -0400

    spring boot added and playing around with html

---
+++ SmartStone/src/main/java/zipcoder/Min/SmartStone/SmartStoneApplication.java
@@ -0,0 +1,13 @@
+package zipcoder.Min.SmartStone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStoneApplication.class, args);
+	}
+
+}
---
+++ SmartStone/src/test/java/zipcoder/Min/SmartStone/SmartStoneApplicationTests.java
@@ -0,0 +1,13 @@
+package zipcoder.Min.SmartStone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
